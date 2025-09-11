dffadc0f2d0ea7a633bde025c4091ec38babca31
arnold-aswan <fredyaswan@gmail.com>
2025-09-11 14:17:28 +0300

    initial project setup

---
+++ server/src/index.ts
@@ -0,0 +1,30 @@
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+
+// ERROR MIDDLEWARE
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	console.error(err.stack, "Error occurred");
+	res.status(500).json({ message: "Internal Server Error" });
+	next();
+});
+
+// NOT FOUND MIDDLEWARE
+app.use((req: Request, res: Response) => {
+	res.status(404).json({ message: "Route not found" });
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
