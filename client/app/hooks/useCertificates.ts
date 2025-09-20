7fa6a2c134392ba0c1727c78ee1cd45650087959
arnold-aswan <fredyaswan@gmail.com>
2025-09-20 17:01:13 +0300

    feat: add certifications page,  hook and table

---
+++ client/app/hooks/useCertificates.ts
@@ -0,0 +1,10 @@
+import { fetchData } from "@/lib/axios-utils";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+export const useGetCertificates = (page: number, limit: number) => {
+	return useQuery({
+		queryKey: ["certificates", page, limit],
+		queryFn: () => fetchData(`/certificates?page=${page}&limit=${limit}`),
+		staleTime: 10 * 60 * 1000,
+	});
+};
--- client/app/routes/dashboard/certifications/index.tsx
+++ client/app/routes/dashboard/certifications/index.tsx
@@ -1,6 +1,43 @@
-import React from "react";
+import {
+	// useDownloadCertificate,
+	useGetCertificates,
+} from "@/hooks/useCertificates";
+import React, { useState } from "react";
+import {
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableFooter,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import { certificatesTableHeads } from "@/constants/data";
+import { Button } from "@/components/ui/button";
+import { Download } from "@/assets/icons";
+import type { CertificatesResponse } from "@/types/types";
+import { certificateStatus, cn, complianceColor } from "@/lib/utils";
+import Pagination from "@/components/shared/Pagination";
+import { format } from "date-fns";
 
 const Certifications = () => {
+	const [page, setPage] = useState(1);
+	const limit = 10;
+
+	const { data: certificatesData, isPending } = useGetCertificates(
+		page,
+		limit
+	) as {
+		data: CertificatesResponse;
+		isPending: boolean;
+	};
+
+	// const { mutate: downloadCertificate, isPending: isDownloading } =
+	// 	useDownloadCertificate();
+
+	console.log(certificatesData);
+	if (isPending) return <div>Loading...</div>;
 	return (
 		<section className="space-y-6">
 			<div>
@@ -9,6 +46,77 @@ const Certifications = () => {
 					<span className="text-muted-foreground text-sm">
 						View and manage organic certification certificates.
 					</span>
+				</div>
+			</div>
+
+			<div className="w-full max-w-full overflow-hidden">
+				<div
+					className="overflow-x-auto border rounded-lg"
+					style={{
+						WebkitOverflowScrolling: "touch",
+						overscrollBehaviorX: "contain",
+						maxWidth: "calc(100vw - 2rem)",
+					}}
+				>
+					<div className="min-w-full">
+						<Table className="w-full  border-collapse">
+							<TableCaption>List of all farms.</TableCaption>
+							<TableHeader>
+								<TableRow>
+									{certificatesTableHeads?.map((data) => (
+										<TableHead className="table-head whitespace-nowrap">
+											{data.value}
+										</TableHead>
+									))}
+								</TableRow>
+							</TableHeader>
+							<TableBody>
+								{certificatesData?.certificates?.map((data) => (
+									<TableRow key={data._id}>
+										<TableCell>{data.certificateNo}</TableCell>
+										<TableCell>{data.farmName}</TableCell>
+										<TableCell>{data.farmerName}</TableCell>
+										<TableCell>{format(data.issueDate, "P")}</TableCell>
+										<TableCell>{format(data.expiryDate, "P")}</TableCell>
+										<TableCell
+											className={cn(
+												complianceColor(data.complianceScore),
+												"text-center"
+											)}
+										>
+											{data.complianceScore}%
+										</TableCell>
+										<TableCell>{certificateStatus(data.expiryDate)}</TableCell>
+										<TableCell className="flex items-center gap-4">
+											<Button
+												size="sm"
+												className="bg-transparent text-black modal-close-btn"
+												// onClick={() => downloadCertificate(data._id)}
+												// disabled={isDownloading}
+											>
+												<Download />
+											</Button>
+										</TableCell>
+									</TableRow>
+								))}
+							</TableBody>
+							<TableFooter>
+								<TableRow>
+									<TableCell colSpan={certificatesTableHeads.length}>
+										<div className="flex justify-center">
+											{certificatesData?.pagination && (
+												<Pagination
+													currentPage={certificatesData.pagination.page}
+													totalPages={certificatesData.pagination.totalPages}
+													onPageChange={(newPage) => setPage(newPage)}
+												/>
+											)}
+										</div>
+									</TableCell>
+								</TableRow>
+							</TableFooter>
+						</Table>
+					</div>
 				</div>
 			</div>
 		</section>
